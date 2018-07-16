c9f0a181347cb9752ce036fe7a7152d5a2309030
Suraj Shetty <surajshetty3416@gmail.com>
2018-07-16 10:44:52 +0530

    Add tree component (first-cut)

---
+++ ui/components/Tree.vue
@@ -0,0 +1,67 @@
+<template>
+  <li>
+    <div
+      class="tree-node"
+      @click="toggleChildren">
+      <span class="tree-node-icon" v-if="isFolder">
+        <feather-icon :name="isFolderOpen ? 'folder-minus': 'folder-plus'"></feather-icon>
+      </span>
+      {{ model.name }}
+    </div>
+    <ul v-show="isFolderOpen" v-if="isFolder">
+      <tree
+        class="tree"
+        v-for="(model, index) in model.children"
+        :key="index"
+        :model="model">
+      </tree>
+    </ul>
+  </li>
+</template>
+<script>
+export default {
+  props: ['model'],
+  computed: {
+    isFolder: function() {
+      return this.model.children && this.model.children.length;
+    }
+  },
+  methods: {
+    toggleChildren: function() {
+      if (this.isFolder) {
+        this.isFolderOpen = !this.isFolderOpen;
+      }
+    },
+    addChild: function() {
+      //
+    }
+  },
+  data() {
+    return {
+      isFolderOpen: false,
+    };
+  }
+};
+</script>
+<style scoped>
+.item {
+  cursor: pointer;
+}
+.bold {
+  font-weight: bold;
+}
+ul {
+  padding-left: 1em;
+  line-height: 1.5em;
+  list-style-type: dot;
+}
+li {
+  list-style: none;
+}
+.tree-node-icon {
+  vertical-align: middle
+}
+.tree-node{
+  margin: 5px;
+}
+</style>
--- ui/plugins/frappeVue.js
+++ ui/plugins/frappeVue.js
@@ -11,6 +11,7 @@ import Button from '../components/Button';
 import Indicator from '../components/Indicator';
 import modalPlugin from '../components/Modal/plugin';
 import formModalPlugin from '../plugins/formModal';
+import Tree from '../components/Tree'
 
 export default function installFrappePlugin(Vue) {
   Vue.component('not-found', NotFound);
@@ -18,6 +19,7 @@ export default function installFrappePlugin(Vue) {
   Vue.component('frappe-control', FrappeControl);
   Vue.component('f-button', Button);
   Vue.component('indicator', Indicator);
+  Vue.component('tree', Tree)
 
   Vue.use(modalPlugin);
   Vue.use(formModalPlugin);
